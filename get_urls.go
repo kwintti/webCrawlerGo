aebf2c3948957fa7f8c82fc75f8fcf572714cda4
kwintti <sergei.borovskii@gmail.com>
2025-07-10 01:05:41 +0300

    only add inside links, normalize before adding to the list

--- get_urls.go
+++ get_urls.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"net/url"
 	"strings"
 
 	"golang.org/x/net/html"
@@ -13,19 +14,34 @@ func getURLsFromHTML(htmlBody, rawBaseURL string) ([]string, error) {
 	if err != nil {
 		return nil, err
 	}
+	base, err := url.Parse(rawBaseURL)
+	if err != nil {
+		return nil, err
+	}
 
 	urls := []string{}
 
 	for n := range doc.Descendants() {
 		if n.Type == html.ElementNode && n.DataAtom == atom.A {
 			for _, a := range n.Attr {
+				if a.Key == "" || a.Key[0] == '#' {
+					continue
+				}
 				if a.Key == "href" {
-					if a.Val[0] == byte('/') {
-						urls = append(urls, rawBaseURL + a.Val)
-						break
+					// if a.Val[0] == byte('/') {
+					// 	urls = append(urls, rawBaseURL + a.Val)
+					// 	break
+					// }
+					// urls = append(urls, a.Val)
+					// break
+					u, err := url.Parse(a.Val)
+					if err != nil {
+						continue
 					}
-					urls = append(urls, a.Val)
-					break
+					resolved := base.ResolveReference(u)
+					if resolved.Host == base.Host {
+						urls = append(urls, resolved.String())
+					}
 				}
 			}
 		}
